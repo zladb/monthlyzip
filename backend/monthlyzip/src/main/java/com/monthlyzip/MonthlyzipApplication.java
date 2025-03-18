1d73ba0a6b1e39a398750a8fc67c0a5d3231827b
Yujin KIM <zla_db@naver.com>
2025-03-18 16:56:54 +0900

    chore: 프로젝트 ë명 fintech->monthlyzip ì으로 변경. resources/sql/schema.sql 추가

---
+++ backend/monthlyzip/src/main/java/com/monthlyzip/MonthlyzipApplication.java
@@ -0,0 +1,13 @@
+package com.monthlyzip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonthlyzipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonthlyzipApplication.class, args);
+	}
+
+}
