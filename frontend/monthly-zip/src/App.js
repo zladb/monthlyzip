c2a3210e82f81e69ab695f8ceff52680d9982492
최연지 <rokmc3038@naver.com>
2025-03-31 09:39:26 +0900

    feat:경로 수정2

--- frontend/monthly-zip/src/App.js
+++ frontend/monthly-zip/src/App.js
@@ -7,8 +7,8 @@ import TenantMgmt from './features/tenantMgmt/TenantMgmt';
 import TenantMgmtDetail from './features/tenantMgmt/TenantMgmtDetail';
 import PaymentList from './features/tenant/payment/PaymentList';
 import PaymentDetail from './features/tenant/payment/PaymentDetail';
-import LandlordMypage from './features/myPage/admin/LandlordMypage';
-import TenantMypage from './features/myPage/user/TenantMypage';
+import LandlordMypage from './features/myPage/landlord/LandlordMypage';
+import TenantMypage from './features/myPage/tenant/TenantMypage';
 
 
 function App() {
@@ -18,10 +18,10 @@ function App() {
         <Route path="/" element={<h1>홈 페이지</h1>} /> 
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/payment-list" element={<PaymentList />} /> 
-        <Route path="/payment-detail" element={<PaymentDetail />} />
-        <Route path="/tenantMgmt" element={<TenantMgmt />} />  
-        <Route path="/tenantMgmtDetail" element={<TenantMgmtDetail />} />
+        <Route path="/tenant/payment-list" element={<PaymentList />} /> 
+        <Route path="/tenant/payment-detail" element={<PaymentDetail />} />
+        <Route path="/landlord/tenant-mgmt" element={<TenantMgmt />} />  
+        <Route path="/landlord/tenant-mgmt-detail" element={<TenantMgmtDetail />} />
         <Route path="/landlord/mypage" element={<LandlordMypage />} />
         <Route path="/tenant/mypage" element={<TenantMypage />} />
        
