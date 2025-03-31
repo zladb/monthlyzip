0b6ded39a676e7c4935e2f2f59864997b1e058a4
Yujin KIM <zla_db@naver.com>
2025-03-31 10:25:02 +0900

    fix: 회원가입 역직렬화 실패 에러 해결 + 에러 처리 로직 추가

--- backend/monthlyzip/src/main/java/com/monthlyzip/global/common/exception/handler/GlobalExceptionHandler.java
+++ backend/monthlyzip/src/main/java/com/monthlyzip/global/common/exception/handler/GlobalExceptionHandler.java
@@ -2,13 +2,17 @@ package com.monthlyzip.global.common.exception.handler;
 
 import com.monthlyzip.global.common.exception.exception.BusinessException;
 import com.monthlyzip.global.common.model.dto.ApiResponse;
+import com.monthlyzip.global.common.model.dto.ApiResponseStatus;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 @Order(0)
+@Slf4j
 public class GlobalExceptionHandler {
 
     // ✅ 비즈니스 예외 처리 (AOP에서 이미 로그 출력됨)
@@ -30,5 +34,17 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ApiResponse<Void>> handleGeneralException(Exception e) {
         return ResponseEntity.internalServerError()
                 .body(ApiResponse.fail(500, "서버 오류가 발생했습니다."));
+    }
+
+    // ✅ 컨트롤러 메서드 실행전 오류는 AOP가 잡아낼 수 없으므로 여기서 직접 로그찍고 fail 반환
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ApiResponse<Void>> handleHttpMessageNotReadable(HttpMessageNotReadableException e) {
+        // 로그 남기고
+        log.error("❌ JSON 역직렬화 실패: {}", e.getMessage());
+
+        // BusinessException 형태로 포장해 통일
+        return ResponseEntity
+                .status(ApiResponseStatus.INVALID_JSON.getHttpStatus())
+                .body(ApiResponse.fail(ApiResponseStatus.INVALID_JSON));
     }
 }
