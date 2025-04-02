901c6cddcd447f7d1d242dfe3248519eb61f5bbb
최연지 <rokmc3038@naver.com>
2025-04-02 16:45:14 +0900

    feat: 마이페이지 작업중

--- frontend/monthly-zip/src/features/myPage/landlord/LandlordMypage.jsx
+++ frontend/monthly-zip/src/features/myPage/landlord/LandlordMypage.jsx
@@ -22,6 +22,8 @@ const LandlordMypage = () => {
         rel="stylesheet"
       />
       <main className={styles.container}>
+
+        {/* 화살표 */}
         <button className={styles.backButton}>
           <svg
             width="27"
@@ -40,113 +42,26 @@ const LandlordMypage = () => {
 
         <h1 className={styles.pageTitle}>MY PAGE</h1>
 
-        <section className={styles.profileSection}>
-          <div className={styles.profileImageContainer}>
-            <svg
-              width="141"
-              height="146"
-              viewBox="0 0 141 146"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              className={styles.profileCircle}
-            >
-              <g filter="url(#filter0_dd_1_5370)">
-                <ellipse
-                  cx="70.5"
-                  cy="72"
-                  rx="66.5"
-                  ry="66"
-                  fill="white"
-                ></ellipse>
-              </g>
-              <defs>
-                <filter
-                  id="filter0_dd_1_5370"
-                  x="0"
-                  y="0"
-                  width="141"
-                  height="146"
-                  filterUnits="userSpaceOnUse"
-                  colorInterpolationFilters="sRGB"
-                >
-                  <feFlood
-                    floodOpacity="0"
-                    result="BackgroundImageFix"
-                  ></feFlood>
-                  <feColorMatrix
-                    in="SourceAlpha"
-                    type="matrix"
-                    values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-                    result="hardAlpha"
-                  ></feColorMatrix>
-                  <feOffset dy="-2"></feOffset>
-                  <feGaussianBlur stdDeviation="2"></feGaussianBlur>
-                  <feComposite in2="hardAlpha" operator="out"></feComposite>
-                  <feColorMatrix
-                    type="matrix"
-                    values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
-                  ></feColorMatrix>
-                  <feBlend
-                    mode="normal"
-                    in2="BackgroundImageFix"
-                    result="effect1_dropShadow_1_5370"
-                  ></feBlend>
-                  <feColorMatrix
-                    in="SourceAlpha"
-                    type="matrix"
-                    values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-                    result="hardAlpha"
-                  ></feColorMatrix>
-                  <feOffset dy="4"></feOffset>
-                  <feGaussianBlur stdDeviation="2"></feGaussianBlur>
-                  <feComposite in2="hardAlpha" operator="out"></feComposite>
-                  <feColorMatrix
-                    type="matrix"
-                    values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
-                  ></feColorMatrix>
-                  <feBlend
-                    mode="normal"
-                    in2="effect1_dropShadow_1_5370"
-                    result="effect2_dropShadow_1_5370"
-                  ></feBlend>
-                  <feBlend
-                    mode="normal"
-                    in="SourceGraphic"
-                    in2="effect2_dropShadow_1_5370"
-                    result="shape"
-                  ></feBlend>
-                </filter>
-              </defs>
-            </svg>
-          </div>
 
-          <div className={styles.profileIconContainer}>
-            <svg
-              width="75"
-              height="74"
-              viewBox="0 0 75 74"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              className={styles.profileIcon}
-            >
-              <g clipPath="url(#clip0_1_5371)">
-                <path
-                  d="M37.5 37C32.3438 37 27.9297 35.1885 24.2578 31.5656C20.5859 27.9427 18.75 23.5875 18.75 18.5C18.75 13.4125 20.5859 9.05729 24.2578 5.43438C27.9297 1.81146 32.3438 0 37.5 0C42.6562 0 47.0703 1.81146 50.7422 5.43438C54.4141 9.05729 56.25 13.4125 56.25 18.5C56.25 23.5875 54.4141 27.9427 50.7422 31.5656C47.0703 35.1885 42.6562 37 37.5 37ZM0 74V61.05C0 58.4292 0.683594 56.0203 2.05078 53.8234C3.41797 51.6266 5.23438 49.95 7.5 48.7938C12.3438 46.4042 17.2656 44.612 22.2656 43.4172C27.2656 42.2224 32.3438 41.625 37.5 41.625C42.6562 41.625 47.7344 42.2224 52.7344 43.4172C57.7344 44.612 62.6562 46.4042 67.5 48.7938C69.7656 49.95 71.582 51.6266 72.9492 53.8234C74.3164 56.0203 75 58.4292 75 61.05V74H0ZM9.375 64.75H65.625V61.05C65.625 60.2021 65.4102 59.4313 64.9805 58.7375C64.5508 58.0438 63.9844 57.5042 63.2812 57.1188C59.0625 55.0375 54.8047 53.4766 50.5078 52.4359C46.2109 51.3953 41.875 50.875 37.5 50.875C33.125 50.875 28.7891 51.3953 24.4922 52.4359C20.1953 53.4766 15.9375 55.0375 11.7188 57.1188C11.0156 57.5042 10.4492 58.0438 10.0195 58.7375C9.58984 59.4313 9.375 60.2021 9.375 61.05V64.75ZM37.5 27.75C40.0781 27.75 42.2852 26.8443 44.1211 25.0328C45.957 23.2214 46.875 21.0438 46.875 18.5C46.875 15.9563 45.957 13.7786 44.1211 11.9672C42.2852 10.1557 40.0781 9.25 37.5 9.25C34.9219 9.25 32.7148 10.1557 30.8789 11.9672C29.043 13.7786 28.125 15.9563 28.125 18.5C28.125 21.0438 29.043 23.2214 30.8789 25.0328C32.7148 26.8443 34.9219 27.75 37.5 27.75Z"
-                  fill="#C0C0C0"
-                ></path>
-              </g>
-              <defs>
-                <clipPath id="clip0_1_5371">
-                  <rect width="75" height="74" fill="white"></rect>
-                </clipPath>
-              </defs>
-            </svg>
-          </div>
+      {/* 프로필 이미지 */}
+      <section className={styles.profileSection}>
+        <div className={styles.profileIconWrapper}>
+          <div
+            dangerouslySetInnerHTML={{
+              __html:
+                "<svg id=\"352:6419\" width=\"150\" height=\"163\" viewBox=\"0 0 150 163\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\" class=\"profile-icon\" style=\"width: 100%; height: 100%\"> <g filter=\"url(#filter0_dd_352_6419)\"> <ellipse cx=\"70.5\" cy=\"72\" rx=\"66.5\" ry=\"66\" fill=\"white\"></ellipse> </g> <g clip-path=\"url(#clip0_352_6419)\"> <path d=\"M70.5 72C65.3438 72 60.9297 70.1885 57.2578 66.5656C53.5859 62.9427 51.75 58.5875 51.75 53.5C51.75 48.4125 53.5859 44.0573 57.2578 40.4344C60.9297 36.8115 65.3438 35 70.5 35C75.6562 35 80.0703 36.8115 83.7422 40.4344C87.4141 44.0573 89.25 48.4125 89.25 53.5C89.25 58.5875 87.4141 62.9427 83.7422 66.5656C80.0703 70.1885 75.6562 72 70.5 72ZM33 109V96.05C33 93.4292 33.6836 91.0203 35.0508 88.8234C36.418 86.6266 38.2344 84.95 40.5 83.7938C45.3438 81.4042 50.2656 79.612 55.2656 78.4172C60.2656 77.2224 65.3438 76.625 70.5 76.625C75.6562 76.625 80.7344 77.2224 85.7344 78.4172C90.7344 79.612 95.6562 81.4042 100.5 83.7938C102.766 84.95 104.582 86.6266 105.949 88.8234C107.316 91.0203 108 93.4292 108 96.05V109H33ZM42.375 99.75H98.625V96.05C98.625 95.2021 98.4102 94.4313 97.9805 93.7375C97.5508 93.0438 96.9844 92.5042 96.2812 92.1188C92.0625 90.0375 87.8047 88.4766 83.5078 87.4359C79.2109 86.3953 74.875 85.875 70.5 85.875C66.125 85.875 61.7891 86.3953 57.4922 87.4359C53.1953 88.4766 48.9375 90.0375 44.7188 92.1188C44.0156 92.5042 43.4492 93.0438 43.0195 93.7375C42.5898 94.4313 42.375 95.2021 42.375 96.05V99.75ZM70.5 62.75C73.0781 62.75 75.2852 61.8443 77.1211 60.0328C78.957 58.2214 79.875 56.0438 79.875 53.5C79.875 50.9563 78.957 48.7786 77.1211 46.9672C75.2852 45.1557 73.0781 44.25 70.5 44.25C67.9219 44.25 65.7148 45.1557 63.8789 46.9672C62.043 48.7786 61.125 50.9563 61.125 53.5C61.125 56.0438 62.043 58.2214 63.8789 60.0328C65.7148 61.8443 67.9219 62.75 70.5 62.75Z\" fill=\"#C0C0C0\"></path> </g> <g filter=\"url(#filter1_d_352_6419)\"> <circle cx=\"117\" cy=\"127.092\" r=\"20\" fill=\"#FE6A00\"></circle> <text fill=\"white\" xml:space=\"preserve\" style=\"white-space: pre\" font-family=\"Inter\" font-size=\"31\" letter-spacing=\"0em\"><tspan x=\"106.909\" y=\"137.273\">+</tspan></text> </g> <defs> <filter id=\"filter0_dd_352_6419\" x=\"0\" y=\"0\" width=\"141\" height=\"146\" filterUnits=\"userSpaceOnUse\" color-interpolation-filters=\"sRGB\"> <feFlood flood-opacity=\"0\" result=\"BackgroundImageFix\"></feFlood> <feColorMatrix in=\"SourceAlpha\" type=\"matrix\" values=\"0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0\" result=\"hardAlpha\"></feColorMatrix> <feOffset dy=\"-2\"></feOffset> <feGaussianBlur stdDeviation=\"2\"></feGaussianBlur> <feComposite in2=\"hardAlpha\" operator=\"out\"></feComposite> <feColorMatrix type=\"matrix\" values=\"0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0\"></feColorMatrix> <feBlend mode=\"normal\" in2=\"BackgroundImageFix\" result=\"effect1_dropShadow_352_6419\"></feBlend> <feColorMatrix in=\"SourceAlpha\" type=\"matrix\" values=\"0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0\" result=\"hardAlpha\"></feColorMatrix> <feOffset dy=\"4\"></feOffset> <feGaussianBlur stdDeviation=\"2\"></feGaussianBlur> <feComposite in2=\"hardAlpha\" operator=\"out\"></feComposite> <feColorMatrix type=\"matrix\" values=\"0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0\"></feColorMatrix> <feBlend mode=\"normal\" in2=\"effect1_dropShadow_352_6419\" result=\"effect2_dropShadow_352_6419\"></feBlend> <feBlend mode=\"normal\" in=\"SourceGraphic\" in2=\"effect2_dropShadow_352_6419\" result=\"shape\"></feBlend> </filter> <filter id=\"filter1_d_352_6419\" x=\"92\" y=\"104.092\" width=\"58\" height=\"58\" filterUnits=\"userSpaceOnUse\" color-interpolation-filters=\"sRGB\"> <feFlood flood-opacity=\"0\" result=\"BackgroundImageFix\"></feFlood> <feColorMatrix in=\"SourceAlpha\" type=\"matrix\" values=\"0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0\" result=\"hardAlpha\"></feColorMatrix> <feOffset dx=\"4\" dy=\"6\"></feOffset> <feGaussianBlur stdDeviation=\"4.5\"></feGaussianBlur> <feComposite in2=\"hardAlpha\" operator=\"out\"></feComposite> <feColorMatrix type=\"matrix\" values=\"0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0\"></feColorMatrix> <feBlend mode=\"normal\" in2=\"BackgroundImageFix\" result=\"effect1_dropShadow_352_6419\"></feBlend> <feBlend mode=\"normal\" in=\"SourceGraphic\" in2=\"effect1_dropShadow_352_6419\" result=\"shape\"></feBlend> </filter> <clipPath id=\"clip0_352_6419\"> <rect width=\"75\" height=\"74\" fill=\"white\" transform=\"translate(33 35)\"></rect> </clipPath> </defs> </svg>",
+            }}
+          />
+        </div>
+      </section>
 
-          <p className={styles.userType}>임대인</p>
-        </section>
+      {/* role */}
+      <p className={styles.userType}>임대인</p>
 
+  
         <form className={styles.formContainer}>
+          
+          {/* 이메일 */}
           <div className={styles.formGroup}>
             <label htmlFor="email" className={styles.formLabel}>
               이메일 *
@@ -162,6 +77,7 @@ const LandlordMypage = () => {
             </div>
           </div>
 
+          {/* 비밀번호 */}
           <div className={styles.formGroup}>
             <label htmlFor="password" className={styles.formLabel}>
               비밀번호 *
@@ -176,6 +92,7 @@ const LandlordMypage = () => {
             </div>
           </div>
 
+          {/* 비밀번호 확인 */}
           <div className={styles.formGroup}>
             <label htmlFor="confirmPassword" className={styles.formLabel}>
               비밀번호 확인 *
@@ -190,6 +107,7 @@ const LandlordMypage = () => {
             </div>
           </div>
 
+          {/* 이름 */}
           <div className={styles.formGroup}>
             <label htmlFor="name" className={styles.formLabel}>
               이름 *
@@ -205,6 +123,7 @@ const LandlordMypage = () => {
             </div>
           </div>
 
+          {/* 전화번호 */}
           <div className={styles.formGroup}>
             <label htmlFor="phone" className={styles.formLabel}>
               휴대폰 번호 *
@@ -220,7 +139,8 @@ const LandlordMypage = () => {
             </div>
           </div>
         </form>
-
+        
+        {/* 버튼 */}
         <button type="submit" className={styles.submitButton}>
           수정
         </button>
