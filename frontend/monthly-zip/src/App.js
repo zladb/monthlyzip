6d88d849ea907a06775c4d4683fe9009e0fa3f81
이지현 <w4277@naver.com>
2025-04-02 10:42:42 +0900

    feat:임차인 공지시항 상세 화면 구현
    S12P21D109-152 #done 작업완료

--- frontend/monthly-zip/src/App.js
+++ frontend/monthly-zip/src/App.js
@@ -14,6 +14,7 @@ import NoticeList from './features/tenant/notice/NoticeList';
 import ContractInformation from './features/tenant/contract/ContractInformation';
 import AutoPaymentAgreement from './features/tenant/payment/auto/AutoPaymentAgreement';
 import AutoPayment from './features/tenant/payment/auto/AutoPayment';
+import NoticeDetail from './features/tenant/notice/NoticeDetail';
 
 
 function App() {
@@ -28,13 +29,15 @@ function App() {
         <Route path="/tenant/payment-main" element={<PaymentMain />} />
         <Route path="/tenant/auto-payment" element={<AutoPayment />} />
         <Route path="/tenant/auto-payment-agreement" element={<AutoPaymentAgreement />} /> 
-
+        <Route path="/tenant/contract" element={<ContractInformation />} />
+        <Route path="/tenant/notice-list" element={<NoticeList />} />
+        {/* <Route path="/tenant/notice-detail/:noticeId" element={<NoticeDetail />} /> */}
+        <Route path="/tenant/notice-detail" element={<NoticeDetail />} />
+        
         <Route path="/landlord/tenant-mgmt" element={<TenantMgmt />} />  
         <Route path="/landlord/tenant-mgmt-detail" element={<TenantMgmtDetail />} />
         <Route path="/landlord/mypage" element={<LandlordMypage />} />
         <Route path="/tenant/mypage" element={<TenantMypage />} />
-        <Route path="/tenant/notice-list" element={<NoticeList />} />
-        <Route path="/tenant/contract" element={<ContractInformation />} />
 
 
       </Routes>
